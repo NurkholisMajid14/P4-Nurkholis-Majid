05e5bd95f6f90cbae1b6ddada7f7506866126995
Nurkholis Majid <64547146+NurkholisMajid14@users.noreply.github.com>
2020-05-11 02:03:08 -0700

    Create Jobsheet 5 C.c
    Buatlah program perkalian matriks dua dimensi dengan menggunakan dua NIM mahasiswa

---
+++ Jobsheet 5 C.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+main()
+{ 
+	printf("Nama : Nurkholis Majid\n");
+	printf("Nim : 12\n");
+	printf("Kelompok : 24\n\n");
+
+int x,y;
+int A[2][2];
+int B[2][2];
+int C[2][2];
+printf("PROGRAM PERKALIAN MATRIKS 2x2\n");
+printf("\nInput elemen matriks A : \n");
+for (x=0;x<2;x++){
+for(y=0;y<2;y++){
+printf("A[%d,%d] = ",x,y);
+scanf("%d", &A[x][y]);
+}
+}
+printf("\nInput elemen matriks B : \n");
+for (x=0;x<2;x++)
+{ for(y=0;y<2;y++)
+{ printf("B[%d,%d] = ",x,y);
+scanf("%d", &B[x][y]);}
+}
+printf("\n");
+for (x=0;x<2;x++)
+{ for(y=0;y<2;y++)
+{ C[0][0]=(A[0][0]*B[0][0])+(A[0][1]*B[1][0]);
+C[0][1]=(A[0][0]*B[0][1])+(A[0][1]*B[1][1]);
+C[1][0]=(A[1][0]*B[0][0])+(A[1][1]*B[1][0]);
+C[1][1]=(A[1][0]*B[0][1])+(A[1][1]*B[1][1]);}
+}
+printf("\n A x B =\n");
+for (x=0;x<1;x++)
+{ for(y=0;y<1;y++)
+{ printf("\n |%d %d| x |%d %d| = |%d %d|\n",
+A[0][0],A[0][1],B[0][0],B[0][1],C[0][0],C[0][1]);
+printf(" |%d %d| x |%d %d| = |%d %d|",
+A[1][0],A[1][1],B[1][0],B[1][1],C[1][0],C[1][1]);}
+}
+printf("\n");
+}
+
