4f36aef3f3d6cbbee1175f49022369ae2da4d5a1
Nurkholis Majid <64547146+NurkholisMajid14@users.noreply.github.com>
2020-05-11 01:59:20 -0700

    Update Jobsheet 2 C.c
    Buatlah mengisi dan menampilkan elemen array dengan indeks array sesuai dengan NIM terakhir mahasiswa

--- Jobsheet 2 C.c
+++ Jobsheet 2 C.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main()
+main()
 {
-	int nim[5] = {12,10,8,13,12};
+	int x [5]={12,10,8,13,12};
 	printf("Nama : Nurkholis Majid\n");
 	printf("Nim : 12\n");
 	printf("Kelompok : 24\n\n");
 	
-	printf("nim ke- 1 = %s\n",nim[0]);
-	printf("nim ke- 2 = %s\n",nim[1]);
-	printf("nim ke- 3 = %s\n",nim[2]);
-	printf("nim ke- 4 = %s\n",nim[3]);
-	printf("nim ke- 5 = %s\n",nim[4]);
-
+	printf("masukan nim  = ");
+	scanf("%i",&x[0]);
+	printf("masukan nim  = ");
+	scanf("%i",&x[1]);
+    printf("masukan nim  = ");
+	scanf("%i",&x[2]);
+	printf("masukan nim  = ");
+	scanf("%i",&x[3]);
+    printf("masukan nim  = ");
+	scanf("%i",&x[4]);
 }
