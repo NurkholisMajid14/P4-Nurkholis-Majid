9ea564df931a314b444ce0be8671e30c5b8b0322
Nurkholis Majid <64547146+NurkholisMajid14@users.noreply.github.com>
2020-05-11 02:07:40 -0700

    Create Jobsheet 2 C++.cpp
    Buatlah mengisi dan menampilkan elemen array dengan indeks array sesuai dengan NIM terakhir mahasiswa

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+	printf("Nama : Nurkholis Majid\n");
+	printf("Nim : 12\n");
+	printf("Kelompok : 24\n\n");
+	
+	int x [5];
+	cout<<"masukkan nim : ";
+	cin>>x[0];
+	cout<<"masukkan nim : ";
+	cin>>x[1];
+    cout<<"masukkan nim : ";
+	cin>>x[2];
+	cout<<"masukkan nim : ";
+	cin>>x[3];
+    cout<<"masukkan nim : ";
+	cin>>x[4];
+	
+	return 0;}
